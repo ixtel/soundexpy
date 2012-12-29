03a64d63ace552b181d97d087d8f454d09292f8f
Oren Leiman <oleiman@ucsc.edu>
2012-12-28 22:08:06 -0800

    wraps result in JSON

--- soundex.py
+++ soundex.py
@@ -1,5 +1,6 @@
 import os
 import re
+import json
 from flask import Flask
 
 DBL_PATTERN = re.compile(r'(.)\1*')
@@ -29,6 +30,7 @@ def strip_doubles(name):
 @app.route('/encode/<name>')
 def soundex_encode(name):
     #name = strip_doubles(name)
+    raw = name
     root = name[0]
     while True:
         if VOW_PATTERN.search(name):
@@ -51,8 +53,8 @@ def soundex_encode(name):
     name = re.sub('@', '', name, 0)
     while name.__len__() < 4:
         name += '0'
-    name = name[:4]
-    return root + name[1:4]
+    name = root + name[1:4]
+    return json.dumps({'raw': raw, 'encoded': name})
 
 if __name__ == '__main__':
     #Bind to PORT if defined, otherwise default to 5000
